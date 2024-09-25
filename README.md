0c6938c074ba283a4b61dce701805b3bf9ad99cd
Tim Head <betatim@gmail.com>
2024-09-25 15:21:18 +0200

    Tweak docstrings

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # scikit-image-backend-phony
-A simple scikit-image backend. The goal of this backend is to provide
-a basic implementation that can be used to test the dispatching
-mechanism.
+This repository contains an example scikit-image backend. The goal of this
+backend is to provide a basic implementation that can be used to test the
+dispatching mechanism and serve as documentation of how the dispatching
+mehanism in scikit-image works.
+
+This backend only implements one function,
+`skimage.metrics.mean_squared_error`. This keeps things straightforward
+when reading the code.
 
 > This repository should be moved to the scikit-image GitHub org at some
 > point.
@@ -13,3 +18,7 @@ To install this backend checkout the repository and run
 ```
 pip install -e.
 ```
+
+## Using it
+
+To try it out and
--- scikit_image_backend_phony/implementations/__init__.py
+++ scikit_image_backend_phony/implementations/__init__.py
@@ -2,17 +2,33 @@ import importlib
 
 
 def get_implementation(name):
+    """Get this backends implementation of the function
+
+    The name passed to this function will be the full name of the function that
+    is being dispatched. It will include modules and sub-modules.
+
+    The returned function should have the same signature as the function that
+    is being dispatched, raise the same exceptions, etc.
+
+    If the backend does not contain an implementation then this function should
+    return `None`.
+    """
     module_name, func_name = name.rsplit(".", maxsplit=1)
-    mod = importlib.import_module(
-        "scikit_image_backend_phony.implementations." + module_name
-    )
+
+    try:
+        mod = importlib.import_module(
+            "scikit_image_backend_phony.implementations." + module_name
+        )
+    except ModuleNotFoundError as e:
+        return None
+
     func = getattr(mod, func_name, None)
 
     return func
 
 
 def can_has(name, *args, **kwargs):
-    """Evaluate if the backend wants to take a call or not
+    """Evaluate if the backend wants to take a call or not.
 
     Given the name of the function and the arguments with which it was called
     in the frontend, decide if this backend has a suitable implementation.
